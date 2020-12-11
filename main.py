35a9a15f2777b9cb067ae0e82a5a27e4ea0456c8
Rob-Turff <robert_turff@yahoo.com>
2020-12-11 00:21:38 +0000

    initial commit

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-from soco import SoCo
 import soco
 
 main_group = None
 main_player = None
 
-for zone in soco.discover(interface_addr="192.168.1.178"):
+paradise_radio = "aac://https://stream.radioparadise.com/mellow-320"
+
+for zone in soco.discover(interface_addr="192.168.1.50"):
     info = zone.get_speaker_info()
     if info["zone_name"] == "South":
         main_group = zone.group
 
 main_player = main_group.coordinator
 
-print(main_player.get_current_track_info())
-print(main_player.get_current_transport_info())
-print(main_player.music_library.get_favorite_radio_stations(start=0, max_items=100))
+main_player.play_uri(paradise_radio)
