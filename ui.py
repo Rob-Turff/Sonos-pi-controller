47cf43b96e45d83517c8cd73e36afe57a1e9ee18
Rob-Turff <robert_turff@yahoo.com>
2020-12-21 23:42:08 +0000

    added backlight change for playing state

--- ui.py
+++ ui.py
@@ -113,12 +113,14 @@ class UI:
                         option = self.set_now_playing()
                         self.draw.rectangle(((x, 13), (self.width, 13)), 1)
                         self.draw.text((x, 1), option.name, 1, self.font)
-                        self.draw.text((0, 1), '♪', 1, self.font)
+                        self.draw.text((0, 1), 'C', 1, self.font)
                     else:
-                        option = self.menu_options[index - 1]
-                        if index == self.current_menu_option:
-                            self.draw.rectangle(((x-2, y-1), (self.width, y+10)), 1)
-                        self.draw.text((x, y), option.name, 0 if index == self.current_menu_option else 1, self.font)
+                        diff = self.current_menu_option - 2
+                        if index - diff >= 0:
+                            option = self.menu_options[index - 1]
+                            if index == self.current_menu_option:
+                                self.draw.rectangle(((x-2, y-1), (self.width, y+10)), 1)
+                            self.draw.text((x, y), option.name, 0 if index == self.current_menu_option else 1, self.font)
 
                 w, h = self.font.getsize('>')
                 self.draw.text((0, (self.height - h) / 2), '>', 1, self.font)
