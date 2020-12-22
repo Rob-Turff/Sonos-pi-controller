0a1122becdca7089583d1ed7de29215b2ef1a113
Rob-Turff <robert_turff@yahoo.com>
2020-12-22 14:16:13 +0000

    added current playing display

--- controller.py
+++ controller.py
@@ -7,9 +7,6 @@ class Controller:
     def __init__(self):
         self.main_group = None
         self.main_player: SoCo = None
-
-        # self.paradise_radio = "aac://https://stream.radioparadise.com/mellow-320"
-
         # self.ip = "192.168.1.178"
         self.ip = "192.168.1.50"
 
@@ -30,21 +27,17 @@ class Controller:
 
         my_ui = ui.UI(self, station_dict)
         my_ui.start()
-        # self.change_station(self.paradise_radio, True)
 
     def change_station(self, uri, force_radio, title):
         cur_info = self.main_player.get_current_track_info()
-        is_stopped = self.main_player.get_current_transport_info()["current_transport_state"]
-        print(uri)
+        # is_stopped = self.main_player.get_current_transport_info()["current_transport_state"]
         print(cur_info["uri"])
-        print(self.main_player.is_playing_radio)
-        print(self.main_player.get_current_track_info())
-        if cur_info["uri"] == uri and is_stopped == "PLAYING":
-            print("Pausing: " + uri)
-            # self.main_player.pause()
-        else:
-            print("Playing: " + uri)
-            # self.main_player.play_uri(uri=uri, force_radio=force_radio, title=title)
+        # if cur_info["uri"] == uri and is_stopped == "PLAYING":
+        #     print("Pausing: " + uri)
+        #     # self.main_player.pause()
+        # else:
+        print("Playing: " + uri)
+        self.main_player.play_uri(uri=uri, force_radio=force_radio, title=title)
 
     def toggle_play(self):
         is_playing = self.get_playing_state()
