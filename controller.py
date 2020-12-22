ceb736949d7dbfe3c4734a0f042401c0e66bbfef
Rob-Turff <robert_turff@yahoo.com>
2020-12-22 15:51:32 +0000

    moved stations to json file

--- controller.py
+++ controller.py
@@ -2,6 +2,7 @@ import soco
 from soco import SoCo
 from soco.data_structures import DidlAudioBroadcast
 import ui
+import json
 
 class Controller:
     def __init__(self):
@@ -9,6 +10,11 @@ class Controller:
         self.main_player: SoCo = None
         # self.ip = "192.168.1.178"
         self.ip = "192.168.1.50"
+
+    def get_stations(self):
+        with open("stations.json") as file:
+            data = json.load(file)
+            return data
 
     def start(self):
         for zone in soco.discover(interface_addr=self.ip):
@@ -18,35 +24,40 @@ class Controller:
 
         self.main_player = self.main_group.coordinator
 
-        stations = self.main_player.music_library.get_favorite_radio_stations()
-        station_dict = {"BBC Radio 4" : "", "Radio Paradise Direct" : "", "Radio Paradise Mellow" : "", "Radio Paradise Rock" : "", "Radio Paradise World" : "", "Exmouth AiR" : ""}
-        station: DidlAudioBroadcast
-        for station in stations:
-            if station.title in station_dict:
-                station_dict[station.title] = station.get_uri()
+        station_dict = self.get_stations()
+
+        # stations = self.main_player.music_library.get_favorite_radio_stations()
+        #
+        # station_dict = {"BBC Radio 4" : "", "Radio Paradise Direct" : "", "Radio Paradise Mellow" : "", "Radio Paradise Rock" : "", "Radio Paradise World" : "", "Exmouth AiR" : ""}
+        # station: DidlAudioBroadcast
+        # for station in stations:
+        #     if station.title in station_dict:
+        #         station_dict[station.title] = station.get_uri()
+
+
 
         my_ui = ui.UI(self, station_dict)
         my_ui.start()
 
     def change_station(self, uri, force_radio, title):
         cur_info = self.main_player.get_current_track_info()
-        # is_stopped = self.main_player.get_current_transport_info()["current_transport_state"]
-        print(cur_info["uri"])
-        # if cur_info["uri"] == uri and is_stopped == "PLAYING":
-        #     print("Pausing: " + uri)
-        #     # self.main_player.pause()
-        # else:
-        print("Playing: " + uri)
-        self.main_player.play_uri(uri=uri, force_radio=force_radio, title=title)
-
-    def toggle_play(self):
         is_playing = self.get_playing_state()
-        if is_playing:
-            print("Pausing")
-            # self.main_player.pause()
+        print(cur_info["uri"])
+        if cur_info["uri"] in uri and is_playing:
+            print("Pausing: " + uri[0])
+            self.main_player.pause()
         else:
-            print("Playing")
-            # self.main_player.play()
+            print("Playing: " + uri[0])
+            self.main_player.play_uri(uri=uri[0], force_radio=force_radio, title=title)
+
+    # def toggle_play(self):
+    #     is_playing = self.get_playing_state()
+    #     if is_playing:
+    #         print("Pausing")
+    #         # self.main_player.pause()
+    #     else:
+    #         print("Playing")
+    #         # self.main_player.play()
 
     def get_playing_state(self):
         is_stopped = self.main_player.get_current_transport_info()["current_transport_state"]
