9e99c5d26d4b76853b88428cdf584c8ed572a923
Robert Turff <robert_turff@yahoo.com>
2020-12-30 20:08:41 +0000

    test

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 import controller
 import os
 import time
+import urllib.request
+from datetime import datetime
 
-os.chdir("/usr/share/Sonos-pi-controller")
-my_controller = controller.Controller()
+def connect(host='http://google.com'):
+	response = os.system("ping -c 1 192.168.1.50" )
+	if response == 0:
+		return True
+	else:
+		return False
+
+
+def start(self):
+	while not connect():
+		time.sleep(10)
+		print("Failed To Connect at: ")
+		print(datetime.now())
+
+	os.chdir("/usr/share/Sonos-pi-controller")
+	my_controller = controller.Controller()
+
+
+time.sleep(10)
+start()
