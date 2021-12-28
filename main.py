0554ca093b6de01a140afd5da9371243875daa6d
Robert Turff <Robert_turff@yahoo.com>
2021-12-28 11:29:29 +0000

    ip test

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
-import controller
 import os
 import time
-import urllib.request
 from datetime import datetime
 
+import controller
+
+
 def connect(host='http://google.com'):
-	response = os.system("ping -c 1 192.168.1.50" )
-	if response == 0:
-		return True
-	else:
-		return False
+    return True
+    # response = os.system("ping -c 1 192.168.1.50")
+    # if response == 0:
+    #     return True
+    # else:
+    #     return False
 
 
 def start():
-	while not connect():
-		time.sleep(10)
-		print("Failed To Connect at: ")
-		print(datetime.now())
+    while not connect():
+        time.sleep(10)
+        print("Failed To Connect at: ")
+        print(datetime.now())
 
-	os.chdir("/usr/share/Sonos-pi-controller")
-	my_controller = controller.Controller()
+    os.chdir("/usr/share/Sonos-pi-controller")
+    my_controller = controller.Controller()
 
 
 time.sleep(10)
